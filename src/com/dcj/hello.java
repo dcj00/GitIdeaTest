69ea0bf360d73631356a0c8cba249a3c48c3c88f
dcj00 <294487055@qq.com>
2021-06-04 17:45:05 +0800

    Initial commit

---
+++ src/com/dcj/hello.java
@@ -0,0 +1,11 @@
+package com.dcj;
+
+/**
+ * @author dcj
+ * @create 2021-06-04 17:42
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello, idea java git");
+    }
+}
